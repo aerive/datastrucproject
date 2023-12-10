867e81ec966d37a447bda10cfa86afe4699a4cda
aerivel <151955391+aerive@users.noreply.github.com>
2023-12-10 13:05:19 +0800

    Add files via upload
    aku ada buat edit button tapi tak function...

--- src/codingHere/EnhancedCalendar.java
+++ src/codingHere/EnhancedCalendar.java
@@ -7,9 +7,11 @@ import java.util.*;
 
 public class EnhancedCalendar extends JFrame {
 
+    private ArrayList<Event> eventList = new ArrayList<>(); // Use ArrayList instead of List
     private JLabel monthLabel;
     private JPanel calendarPanel;
     private int currentMonth, currentYear;
+    private JButton editButton;
 
     public EnhancedCalendar() {
         setTitle("Enhanced Calendar");
@@ -28,6 +30,7 @@ public class EnhancedCalendar extends JFrame {
         JButton prevButton = new JButton("Previous");
         JButton nextButton = new JButton("Next");
         JButton searchButton = new JButton("Search Events");
+        JButton editButton = new JButton("Edit");
 
         JPanel navPanel = new JPanel(new GridLayout(1, 3));
         navPanel.add(prevButton);
@@ -36,6 +39,7 @@ public class EnhancedCalendar extends JFrame {
 
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         bottomPanel.add(searchButton);
+        bottomPanel.add(editButton);
 
         add(navPanel, BorderLayout.NORTH);
         add(calendarPanel, BorderLayout.CENTER);
@@ -73,6 +77,39 @@ public class EnhancedCalendar extends JFrame {
         });
 
         setVisible(true);
+
+    }
+
+    public class Event {
+
+        private String date;
+        private String title;
+        private String description;
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
     }
 
     private void updateCalendar() {
@@ -95,12 +132,13 @@ public class EnhancedCalendar extends JFrame {
             label.setForeground(Color.BLUE);
             calendarPanel.add(label);
         }
+        int day;
 
         for (int i = 1; i < startDay; i++) {
             calendarPanel.add(new JLabel(""));
         }
 
-        for (int day = 1; day <= numberOfDaysInMonth; day++) {
+        for (day = 1; day <= numberOfDaysInMonth; day++) {
             final int currentDay = day;  // Make day effectively final
             JButton button = new JButton(String.valueOf(day));
             button.addActionListener(e -> {
@@ -108,7 +146,21 @@ public class EnhancedCalendar extends JFrame {
             });
             calendarPanel.add(button);
         }
+        eventList = new ArrayList<>(); // Add this line
+        eventList.clear();
+        String dateString = monthLabel.getText() + " " + day + ", " + currentYear;
 
+        eventList.stream().filter(event -> event.getDate().equals(dateString)).forEach(event -> {
+            JLabel eventLabel = new JLabel(event.getTitle());
+            eventLabel.putClientProperty("event", event);
+            eventLabel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    editEvent(event);
+                }
+            });
+            calendarPanel.add(eventLabel);
+        });
         revalidate();
         repaint();
     }
@@ -132,6 +184,40 @@ public class EnhancedCalendar extends JFrame {
         } else {
             JOptionPane.showMessageDialog(this, "Please enter a valid search term.");
         }
+    }
+
+    private void editEvent(Event event) {
+        // Display dialog boxes for editing event title and description
+        String newTitle = JOptionPane.showInputDialog("Edit title:", event.getTitle());
+        String newDescription = JOptionPane.showInputDialog("Edit description:", event.getDescription());
+
+        // Update the event object with new information
+        event.setTitle(newTitle);
+        event.setDescription(newDescription);
+
+        // Update the calendar display
+        updateCalendar();
+    }
+
+    private void intializeButtons() {
+        editButton.addActionListener(e -> {
+            // Get the clicked event label
+            JLabel clickedLabel = (JLabel) e.getSource();
+
+            // Get the associated event object using your chosen approach (custom properties or data structure)
+            Event event = (Event) clickedLabel.getClientProperty("event"); // or eventMap.get(clickedLabel);
+            // Display a dialog box to the user to enter the updated event details
+            String newTitle = JOptionPane.showInputDialog("Edit title:", event.getTitle());
+            String newDescription = JOptionPane.showInputDialog("Edit description:", event.getDescription());
+
+            // Update the event object
+            event.setTitle(newTitle);
+            event.setDescription(newDescription);
+
+            // Update the calendar display
+            updateCalendar();
+        });
+
     }
 
     public static void main(String[] args) {
